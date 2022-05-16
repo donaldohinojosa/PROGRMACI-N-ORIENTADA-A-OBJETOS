986be31bc9da1d475183ce1fe89b7583648abbcb
donaldo.ramhinojosa <donaldo.ramhinojosa@gmail.com>
2022-05-15 19:51:07 -0500

    .

---
+++ UNIDAD4/ARREGLOS.py
@@ -0,0 +1,31 @@
+
+
+nombre_alumno= ["Paola","Ulises","Donaldo","Fernando"]
+print(nombre_alumno)
+
+autos_marca=["Kia","Chevrolet","Tesla","Ferrari"]
+print(autos_marca)
+
+animales_nombre=["Lula","Coco","Toby","Max","Thor","Leo","Lucas","Rocky"]
+print(animales_nombre)
+
+telefonos=["Motorola","Huawei","Vivo","Samsung","Apple","Xiaomi","ZTE"]
+print(telefonos)
+
+lapices_punta= ["2B","4B","H","HB","2HB","F","2H"]
+print(lapices_punta)
+
+comida= ["Verduras","Frutas","Bebidas","Cereales","Alimentos de origen animal"]
+print(comida)
+
+muebles= ["Abeto","Pino","Nogal","Melamina","Contrachapado","Tableros dm","Aglomerdado"]
+print(muebles)
+
+ropa= ["Chamarra","Playera","Short","Pantalon","Camisa","Calcetines","Falda"]
+print(ropa)
+
+libretas_hojas= ["Raya","Cuadro","Doble raya","En blanco","Cuadro chico"]
+print(libretas_hojas)
+
+lampara_color= ["Rojo","Verde","Cafe","Negro","Blanco","Gris","Azul","Morado","Amarillo"]
+print(lampara_color)
---
+++ UNIDAD4/DICCIONARIOS.py
@@ -0,0 +1,149 @@
+autos_diccio={
+    
+    "Marca":"Kia",
+    "Placa":"QW237667",
+    "Color":"Negro",
+    "Tipo de auto":"Camioneta",
+}
+print(autos_diccio["Placa"])
+
+
+autos_diccio={
+    
+    "Marca":"Kia",
+    "Placa":"QW237667",
+    "Color":"Negro",
+    "Tipo de auto":"Camioneta",
+}
+print(autos_diccio["Placa"])
+
+
+autos_diccio={
+    
+    "Marca":"Kia",
+    "Placa":"QW237667",
+    "Color":"Negro",
+    "Tipo de auto":"Camioneta",
+}
+print(autos_diccio["Placa"])
+
+
+telefonos_diccio=	{
+  
+"Marca":"Motorola",
+"Modelo":"p4",
+"Numero":"4521561542",
+"Color":"Negro",
+"Pulgadas":"5",
+"Grosor":"6cm",
+"Ancho ":"5cm",
+"Altura":"10cm",
+"Camaras":"3",
+"Botones":"3",
+}
+print(telefonos_diccio["Marca"])
+print(telefonos_diccio["Camaras"])
+print(telefonos_diccio["Pulgadas"])
+print(telefonos_diccio["Color"])
+
+
+
+lapices_diccio=	{
+
+"Marca":"Grafito",
+"Punta":"2b",
+"Numero":"3",
+"Color":"Negro",
+"Material":"Cera",
+"Grosor":"1",
+"Altura":"10cm",
+"Forma":"Triangular",
+}
+print(lapices_diccio["Marca"])
+print(lapices_diccio["Material"])
+print(lapices_diccio["Forma"])
+print(lapices_diccio["Color"])
+
+
+
+comida_diccio={
+	
+"Verduras":"Calabaza, Zanahoria, Brocoli, Papa, Jitomate",
+"Frutas":"Manzana,Platano,Guayaba", 
+"Bebidas":"Agua, Refresco, Chocolate, Cerveza",
+"Cereales":"Lentejas, Frijoles, Avena",
+"Alimentos de origen animal":"Carne, Huevos, Queso, Leche",
+}
+print (comida_diccio["Frutas"])
+
+
+muebles_diccio=	{
+
+"Marca":"H",
+"Año de Elaboracion":"2020",
+"Material":"Madera",
+"Color":"Cafe claro",
+"Altura":"2m",
+"Ancho":"45cm",
+"Largo ":"1.80m",
+"Tipo de Madera":"Pino",
+"Puertas":"4",
+"Cajones":"4",
+"Costo":"5000pesos",
+}
+print(muebles_diccio["Material"])
+print(muebles_diccio["Año de Elaboracion"])
+print(muebles_diccio["Altura"])
+print(muebles_diccio["Tipo de Madera"])
+
+
+
+ropa_diccio=	{
+
+"Marca":"HM",
+"Modelo":"2020",
+"Prenda":"Playera"
+"Talla":"Grande",
+"Genero":"H",
+"Color":"Negro",
+"Costo ":"500",
+"Material":"Mezclilla",
+"Epoca":"Invierno",
+}
+print(ropa_diccio["Marca"])
+print(ropa_diccio["	Costo"])
+print(ropa_diccio["Talla"])
+print(ropa_diccio["Prenda"])
+
+
+libretas_diccio={
+	
+"Marca":"School",
+"Color":"Amarillo",
+"Hoja":"Rallas",
+"Tamaño":"Profesional",
+"Tipo de pasta":"Plastico",
+"Numero de Hojas":"Numero de hojas",
+"Costo":"30pesos"
+}
+print(libretas_diccio["Marca"])
+print(libretas_diccio["Hoja"])
+print(libretas_diccio["Tamaño"])
+print(libretas_diccio["Numero de Hojas"])
+
+
+
+lampara_diccio={
+	
+"Marca":"Steren",
+"Color":"Azul",
+"Material":"Rallas",
+"Tamaño":"Grande",
+"Forma":"Cuadrada",
+"Color de luz":"Amarilla",
+"Costo":"400pesos"
+}
+print(lampara_diccio["Marca"])
+print(lampara_diccio["Forma"])
+print(lampara_diccio["Color de luz"])
+print(lampara_diccio["Tamaño"])
